87a2d5ab227d671c66fb36647de3585603690ba2
Dharmen <kr.dharmendra895@gmail.com>
2023-08-09 23:05:46 +0530

    feat:apply dark-light mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import Logo from "./Logo";
-import { GithubIcon, LinkedInIcon,TwitterIcon } from "./Icons";
+import { GithubIcon, LinkedInIcon,SunIcon,TwitterIcon,MoonIcon } from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
+
+
 
 
 
@@ -18,6 +21,7 @@ let CustomLink = ({ href, title, className = "" }) => {
              absolute left-0 -bottom-0.5
             group-hover:w-full transition-[width] ease duration-300
             ${router.asPath === href ? "w-full" : "w-0"}
+            dark:bg-light
             `}
       >
         &nbsp;
@@ -27,8 +31,10 @@ let CustomLink = ({ href, title, className = "" }) => {
 };
 
 const NavBar = () => {
+  const [mode,setMode]=useThemeSwitcher();
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between text-black ">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light
+     ">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -60,6 +66,20 @@ const NavBar = () => {
             <LinkedInIcon />
         </motion.a>
        
+      <button
+       onClick={()=>setMode(mode==="light"?"dark":"light")}
+       className={`ml-3 flex items-center justify-center rounded-full p-1
+       ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+       `}
+       >
+
+        {
+          mode==="dark"?
+          <SunIcon className={"fill-dark"}/>
+          : <MoonIcon className={"fill-dark"}/>
+        }
+
+      </button>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
