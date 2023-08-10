7ad2f2136a0f06cc4439a5017207f81c65c42695
Dharmen <kr.dharmendra895@gmail.com>
2023-08-10 10:32:31 +0530

    feat:apply dark-light mode

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+
 const useThemeSwitcher = () => {
   const preferDarkQuery = "(prefers-color-scheme: dark)";
   const [mode, setMode] = useState("");
@@ -35,8 +36,9 @@ const useThemeSwitcher = () => {
 useEffect(() => {   
     if(mode === "dark") {
         window.localStorage.setItem("theme", "dark");
-        document.documentElement.classList.add("dark");
-    }else{
+        document.documentElement.classList.add("dark")
+    }
+    if(mode === "light"){
         window.localStorage.setItem("theme", "light");
         document.documentElement.classList.remove("dark");
     }
